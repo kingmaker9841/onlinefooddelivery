a839aaa47768fc41ce3d0198b08f374f5580fa9f
Mayesh <josephhernandez9841@gmail.com>
2020-09-29 18:57:25 +0545

    Connect with MongoDBAtlas

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
+//Establishing Database Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}, (err)=>{
+    if (err) return "DB_ERROR";
+    console.log("Mongoose connected...");
+})
+
 const PORT = proces.env.PORT || 5000;
+//Listening on PORT
 app.listen(PORT, ()=>{
     console.log(`Server started on ${PORT}`);
 })
